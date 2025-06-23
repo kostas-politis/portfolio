bf1e075948f0ee241022c68f1932bb2210b94fa8
Konstantinos Politis <74538281+kostas-politis@users.noreply.github.com>
2025-06-23 19:17:17 +0300

    update bullets description

--- components/Timeline/work.ts
+++ components/Timeline/work.ts
@@ -12,10 +12,11 @@ export const workExperience: WorkExperience[] = [
     company: 'Mitel Networks Corp.',
     period: 'October 2023 - Present',
     bullets: [
-      'Contributed to the development of a Node.js REST API enabling internal telecom systems to efficiently manage users and tenants in non-relational databases, streamlining backend operations.',
-      'Led testing efforts for newly released features, including manual testing and automating 70% of test cases with Selenium, providing immediate quality feedback during software releases',
-      'Resolved production defects of varying priority in an Angular based softphone application, enhancing system reliability and user experience for over 60,000 daily users.',
-      'Built and maintained Jenkins pipelines for test automation suites, performing daily result analysis to identify issues and optimize test performance',
+      'Collaborated within a cross-functional 8-person team to develop a microservices-based Unified Communications platform serving over 60,000 daily users.',
+      'Built and maintained a Node.js Express REST API that served as a gateway for internal systems to perform user and tenant management operations on MongoDB databases.',
+      'Developed a responsive Angular admin dashboard that interfaced with backend APIs, providing administrators with essential tools for platform monitoring and configuration.',
+      'Created Jenkins pipelines to automate end-to-end test suites execution, performing daily result analysis to identify and resolve quality issues.',
+      'Improved CI/CD workflows by integrating automated notifications that alert teams of build events through internal collaboration tools.'
     ],
     image: {
       path: '/mitel-logo-60.png',
@@ -27,7 +28,7 @@ export const workExperience: WorkExperience[] = [
     company: 'Atos Unify (Now part of Mitel)',
     period: 'June 2022 - October 2023',
     bullets: [
-      'Conducted comprehensive manual and regression testing  for enterprise-level  collaboration and customer service software, identifying over 150 critical defects efficiently tracked via Jira.',
+      'Conducted manual and regression testing for enterprise-level collaboration and customer service software, identifying over 150 critical defects efficiently tracked via Jira.',
       'Designed and maintained reliable test automation frameworks, developing over 500 test cases for web, Android, and iOS platforms, significantly reducing manual testing effort and improving overall efficiency.',
       'Managed the test environments for internal telecom systems (PBXs), including deployment, configuration and user provisioning, ensuring system stability for testing workflows.',
     ],
